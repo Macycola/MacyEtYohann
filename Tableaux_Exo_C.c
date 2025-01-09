b371b060bd835add29d83ac29b0622cc0aafd9c6
Macycola <macysaoudi@gmail.com>
2025-01-09 12:16:44 +0100

    Add files via upload

--- Tableaux_Exo_C.c
+++ Tableaux_Exo_C.c
@@ -90,3 +90,80 @@ int main() {
 
     return 0;
 }
+
+// Exercice 1.6
+
+#include <stdio.h>
+int main() {
+    int tableau[10]; // Tableau pour stocker 10 nombres
+    int i, nombreRecherche, trouve = 0;
+
+    // Demander à l'utilisateur d'entrer 10 nombres
+    printf("Entrez 10 nombres :\n");
+    for (i = 0; i < 10; i++) {
+       printf("Nombre %d : ", i + 1);
+        scanf("%d", &tableau[i]);
+    }
+     //Demander un nombre à rechercher
+     printf("Entrez le nombre que vous voulez chercher");
+     scanf("%d", &nombreRecherche);
+
+     //Rechercher le nombre dans le tableau
+     for(i=0; i<10; i++){
+            if(tableau[i] == nombreRecherche ){
+                printf("Le nombre %d a été trouvé à l'indice %d.\n", nombreRecherche, i);
+                trouve=1;
+                 }
+                 }
+                 }
+
+// Exercice 1.7
+#include <stdio.h>
+int main() {
+    int tableau[10];
+
+    // Demander à l'utilisateur d'entrer 10 nombres
+    printf("Entrez 10 nombres");
+    for(i=0; i<10; i++){
+        printf("Nombre :", i+1);
+        scanf(tableau[i]);
+        }
+        }
+
+// Exercice 1.7
+#include <stdio.h>
+
+int main() {
+    int tableau[10], i, dernier;
+
+    // Demander à l'utilisateur de saisir 10 nombres
+    printf("Entrez 10 nombres :\n");
+    for (i = 0; i < 10; i++) {
+        printf("Nombre %d : ", i + 1);
+        scanf("%d", &tableau[i]);
+          }
+
+    //Affiche le tableau avant rotation
+
+    printf("\nTableau avant rotation :\n");
+    for (i = 0; i<10; i++){
+printf("%d ", tableau[i]);    }
+    printf("\n");
+
+      // Effectuer une rotation circulaire vers la droite
+    dernier = tableau[9]; // Sauvegarder le dernier élément
+    for (i = 9; i > 0; i--) {
+        tableau[i] = tableau[i - 1];
+    }
+    tableau[0] = dernier; // Placer le dernier élément en première position
+
+    // Afficher le tableau après la rotation
+    printf("\nTableau après rotation :\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d ", tableau[i]);
+    }
+    printf("\n");}
+
+
+
+        
